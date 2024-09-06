794fafba8aab4bc75a04946d0d71764d4e49cd02
Nate Mills <natemills018@gmail.com>
2024-09-05 23:29:25 -0400

    Adding todo componennt

---
+++ src/client/Components/AddTodo.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { GET, POST } from "../services/fetcher";
+import { useNavigate } from "react-router-dom";
+
+interface TodoProps {}
+
+const AddTodo = (props: TodoProps) => {
+  const navigate = useNavigate();
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {});
+
+  const handleButton = () => {
+    const url = "/api/todos/";
+    POST(url, { todo }).then((todo) => {
+      console.log(todo);
+    });
+
+    alert("Thanks for your Feedback!");
+  };
+
+
+  console.log(todo);
+
+  return <div>
+    <div className="container">
+
+        <section className="row justify-content-center">
+            
+            <div className="card-body">
+                <h1 className="d-flex justify-content-center">
+                    Add Your Todo Here!
+                </h1>
+            </div>
+
+            <div className="d-flex justify-content-center m-2">
+                <button className="rounded mx-4 bg-lightgray" onClick={handleButton}>
+                    Click to Add Your Todo
+                </button>
+            </div>
+
+        </section>
+
+
+    </div>
+  </div>;
+};
+
+export default AddTodo;
